dcab74fd50b8c3cf7a0be3d562366b41d2d1ba59
nagadurga-tech <nagadurgarandi073.gmail.com>
2026-07-31 12:47:38 +0530

    Solved Majority Element using Boyer-Moore Voting Algorithm

--- src/Arrays/Traversal/MajorityElement.java
+++ src/Arrays/Traversal/MajorityElement.java
@@ -1,16 +1,14 @@
 /*
 Approach:
 ---------
-1. Sort the array.
-2. Since the majority element appears more than n/2 times,
-   it will always be present at the middle index after sorting.
-3. Return nums[n/2].
-
-Time Complexity : O(n log n)
+1. Keep a candidate and a count.
+2. If count becomes 0, choose the current element as the new candidate.
+3. If the current element is the same as the candidate, increase the count.
+4. Otherwise, decrease the count.
+5. Since the majority element appears more than n/2 times,
+it cannot be completely canceled and will remain as the final candidate.
+Time Complexity: O(n)
 Space Complexity: O(1)
-
-Key Concept:
-Sorting guarantees that the majority element occupies the middle position.
 */
 
 
@@ -18,9 +16,19 @@ Sorting guarantees that the majority element occupies the middle position.
 import java.util.*;
 class MajorityElement {
     public static int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        int n=nums.length/2;
-        return nums[n];   
+       int res=0;
+       int count=0;
+       for(int n:nums){
+            if(count==0){
+                res=n;
+            }
+            if(n==res){
+                count++;
+            }else{
+                count--;
+            }
+       }   
+       return res;
     }
     public static void main(String args[]){
         int[] nums={2,2,1,1,1,2,2};
